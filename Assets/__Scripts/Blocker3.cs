a821e814a7546aae7aacd2d962559d8bd8bf0529
jim <ryhjames@gmail.com>
2020-10-12 18:39:28 -0400

    blocker 3
    blocker3

---
+++ Assets/__Scripts/Blocker3.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocker3 : MonoBehaviour
+{
+
+    //movement speed in units per second
+    private float Speed = 10f;
+
+
+
+
+    Vector3 pointA = new Vector3(30, 3, 0);
+    Vector3 pointB = new Vector3(30, 17, 0);
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
+    }
+}
