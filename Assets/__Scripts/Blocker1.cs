23fe772a84228f00943122fcdcb999f3b0667354
jim <ryhjames@gmail.com>
2020-10-12 18:37:22 -0400

    blocker
    adding a blocker

---
+++ Assets/__Scripts/Blocker1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocker1 : MonoBehaviour
+{
+
+    //movement speed in units per second
+    private float Speed = 4f;
+
+    
+          
+    
+        Vector3 pointA = new Vector3(14, -2, 0);
+        Vector3 pointB = new Vector3(14, 20, 0);
+        void Update()
+        {
+            transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
+        }
+    }
